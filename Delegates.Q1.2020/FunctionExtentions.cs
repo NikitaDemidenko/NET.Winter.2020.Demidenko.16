e94e1d2632b541b907e5e3c3c8c60c7f7825b82c
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-02-25 18:27:04 +0300

    feat: add implementaion for Generic delegates. Predicates task.

---
+++ Delegates.Q1.2020/FunctionExtentions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Delegates.Q1._2020
+{
+    public static class FunctionExtensions
+    {
+        /// <summary>
+        ///   Combines several predicates using logical AND operator 
+        /// </summary>
+        /// <param name="predicates">array of predicates</param>
+        /// <returns>
+        ///   Returns a new predicate that combine the specified predicated using AND operator
+        /// </returns>
+        /// <example>
+        ///   var result = CombinePredicatesWithAnd(new Predicate<string>[] {
+        ///            x => !string.IsNullOrEmpty(x),
+        ///            x => x.StartsWith("START"),
+        ///            x => x.EndsWith("END"),
+        ///            x => x.Contains("#")
+        ///        });
+        ///   should return the predicate that identical to 
+        ///   x=> (!string.IsNullOrEmpty(x)) && x.StartsWith("START") && x.EndsWith("END") && x.Contains("#")
+        ///
+        ///   The following example should create predicate that returns true if int value between -10 and 10:
+        ///   var result = CombinePredicatesWithAnd(new Predicate<int>[] {
+        ///            x => x > -10,
+        ///            x => x < 10
+        ///       });
+        /// </example>
+        public static Predicate<T> CombinePredicatesWithAnd<T>(Predicate<T>[] predicates)
+        {
+            Validation(predicates);
+            
+            return delegate (T item)
+            {
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(item))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            };
+        }
+
+        private static void Validation<T>(Predicate<T>[] predicates)
+        {
+            if (predicates is null)
+            {
+                throw new ArgumentNullException(nameof(predicates));
+            }
+
+            if (predicates.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(predicates)} is empty.");
+            }
+        }
+    }
+}
